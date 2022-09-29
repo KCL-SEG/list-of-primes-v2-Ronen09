e140c37b3bc5b6e4698b844205f093d7cc29b358
Ronen09 <ronen.roy@kcl.ac.uk>
2022-09-29 10:58:16 +0100

    modified primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be greater than 0")
     list = []
+    i = 2;
+    while True:
+        if check_prime(i):
+            list.append(i)
+        if len(list) == number_of_primes:
+            return list
+        i += 1
     return list
+
+def check_prime(number):
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True
